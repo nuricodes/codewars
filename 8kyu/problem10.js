49feaae9166c45e7e579802bbace271aafc929d8
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-01 17:57:53 -0500

    problem 10 complete

---
+++ 8kyu/problem10.js
@@ -0,0 +1,24 @@
+// Given an array of integers your solution should find the smallest integer.
+
+/////////////////////////////////////////////
+// SOLUTION 1
+/////////////////////////////////////////////
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args) // spread operator to show arg as array
+    }
+}
+
+/////////////////////////////////////////////
+// SOLUTION 2
+/////////////////////////////////////////////
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min.apply(null, args)
+    }
+}
+
+/////////////////////////////////////////////
+// SOLUTION 3
+/////////////////////////////////////////////
