6bdf6af643b867c454745497c0b3f90a494f9788
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-04 06:17:15 -0500

    Problem 27 complete

---
+++ 8kyu/return-double.js
@@ -0,0 +1,10 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+/////////////////////////////////////////////
+// SOLUTION 1
+/////////////////////////////////////////////
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
