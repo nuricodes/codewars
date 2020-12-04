fd0328b5e32fb88abc64d52dec32a9f2d7796b69
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-04 12:51:18 -0500

    Problem 31 complete

---
+++ 8kyu/returned_strings.js
@@ -0,0 +1,12 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// SQL: return results in a column named greeting
+
+/////////////////////////////////////////////
+// SOLUTION 1
+/////////////////////////////////////////////
+
+function greet(name) {
+    return `Hello, ${name}, how are you doing today?`
+}
+
