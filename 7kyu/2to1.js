cc69491d6f7a9f8911ae39bf7c7c754f53c253b4
ayninur <nuralaynabdulqadir@gmail.com>
2021-02-04 13:56:58 -0500

    Solution 2

--- 7kyu/2to1.js
+++ 7kyu/2to1.js
@@ -1,5 +1,9 @@
 // Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
+// Solution 2
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("")
+
+
 // Solution 1
 function longest(s1, s2) {
     let joined = [...s1, ...s2];
