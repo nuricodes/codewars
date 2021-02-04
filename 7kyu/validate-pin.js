b8083b9fb5684476453dc21271bc22d117894170
ayninur <nuralaynabdulqadir@gmail.com>
2021-02-04 17:27:36 -0500

    Problem 21: Solution 1 & Solution 2

---
+++ 7kyu/validate-pin.js
@@ -0,0 +1,13 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Solution 1 (doesn't cover all edge cases)
+function validatePIN(pin) {
+    return parseInt(pin) && pin.length === 4 || parseInt(pin) && pin.length === 6 ? true : false
+}
+
+// Solution 2
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin);
+}
